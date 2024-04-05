b9a732abf2bede243ceb7aa91d04d8ae17025a7c
Peng Lyu <penn.lv@gmail.com>
2024-04-04 21:23:33 -0700

    optimization

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,26 +1,18 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-fn check_path(path: &str) {
-    if Path::new(path).is_dir() {
-        // println!("Checking {} ...", path);
 
-        for entry in std::fs::read_dir(path).unwrap() {
-            let entry = entry.unwrap();
-            let path = entry.path();
-            let path = path.to_str().unwrap();
-            // check if file is python or python3
-            // if it's windows
-            if cfg!(windows) {
-                if path.ends_with("python.exe") || path.ends_with("python3.exe") {
-                    println!("{}", path);
-                }
-            } else {
-                if path.ends_with("python") || path.ends_with("python3") {
-                    println!("{}", path);
-                }
-            }
-            
+const PYTHON_SUFFIX_ARRAY: [&str; 4] = if cfg!(windows) {
+    ["python.exe", "python3.exe", "ipython.exe", "ipython3.exe"]
+} else {
+    ["python", "python3", "ipython", "ipython3"]
+};
+
+fn check_path(path: &Path) {
+    for suffix in PYTHON_SUFFIX_ARRAY.iter() {
+        let python_path = path.join(suffix);
+        if python_path.exists() {
+            println!("{}", python_path.display());
         }
     }
 }
@@ -66,7 +58,7 @@ fn main() {
     // println!(paths_from_env)
     let mut handles = vec![];
     for path in paths_from_env {
-        let path = path.to_string();
+        let path = Path::new(path).to_path_buf();
         // println!("Checking {} ...", path);
         let handle = std::thread::spawn(move || {
             check_path(&path);
@@ -77,12 +69,12 @@ fn main() {
     // search for python or python3 in .pyenv versions folder
     let home_env = std::env::var("HOME");
     if home_env.is_ok() {
-        let pyenv = format!("{}/.pyenv", home_env.unwrap());
-        let versions = format!("{}/versions", pyenv);
+        let pyenv = Path::new(&home_env.unwrap()).join(".pyenv");
+        let versions = pyenv.join("versions");
         for entry in std::fs::read_dir(versions).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
-            let bin = format!("{}/bin", path.display());
+            let bin = path.join("bin");
             let handle = std::thread::spawn(move || {
                 check_path(&bin);
             });
@@ -97,7 +89,7 @@ fn main() {
         // print
         if local_app_data.is_ok() {
             // windows store root
-            let windows_store_root = format!("{}\\Microsoft\\WindowsApps", local_app_data.unwrap());
+            let windows_store_root = Path::new(&local_app_data.unwrap()).join("Microsoft").join("WindowsApps");
             let handle = std::thread::spawn(move || {
                 check_path(&windows_store_root);
             });
@@ -121,8 +113,9 @@ fn main() {
             // Consumes the iterator, returns an (Optional) String
             for line in lines {
                 if let Ok(ip) = line {
+                    let ip_path = Path::new(&ip).to_path_buf(); // Clone the value of `ip`
                     let handle = std::thread::spawn(move || {
-                        check_path(&ip);
+                        check_path(&ip_path);
                     });
                     handles.push(handle);
                 }
